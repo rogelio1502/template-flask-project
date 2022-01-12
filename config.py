efef67ce6b51ced632daedcc2c1c4e420ca35cac
rogelio1502 <rogelio15021515@gmail.com>
2022-01-12 15:51:01 -0600

    added some message about db uri

--- config.py
+++ config.py
@@ -8,7 +8,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 #Enable debug mode
 DEBUG = True
 
-# Connect to the database
+# Connect to the database 
+#our database url
 SQLALCHEMY_DATABASE_URI = 'sqlite:////tmp/test.db'
 
 # Turn off the Flask-SQLAlchemy event system and warning
